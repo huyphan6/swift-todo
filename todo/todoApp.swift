130f3bfb07cb68737a0962cec4955d3d9d8978ff
Huy Phan <phan.q.huy21@gmail.com>
2023-11-15 14:04:18 -0500

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Huy Phan on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
