579b34e4a488eea19de36b23cb34c9525fe0692e
Huy Phan <phan.q.huy21@gmail.com>
2023-11-15 15:18:06 -0500

    configured firebase ios sdk

--- todo/todoApp.swift
+++ todo/todoApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct todoApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
