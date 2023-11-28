8304e5d90db6110638d3cdc8abd8c2b0ec15b0b4
Huy Phan <phan.q.huy21@gmail.com>
2023-11-27 21:21:08 -0500

    todo item CRUD functionality
    + todo items are now saved as a sub collection for each user into the database

--- todo/ViewModels/NewItemViewViewModel.swift
+++ todo/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     init() {}
@@ -15,6 +17,35 @@ class NewItemViewViewModel: ObservableObject {
     @Published var showAlert = false
     
     func save() {
+        guard canSave else {
+            return
+        }
+        
+        // get user ID
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId,
+                                   description: description,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        /// save model to DB as a subCollection: each user has their own to do items so we also need to pass in the user's ID
+        /// get an instance of the DB
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos") /// creates a sub collection for the current user
+            .document(newId)
+            .setData(newItem.asDictionary())
+    
+        
+        
         
     }
     
