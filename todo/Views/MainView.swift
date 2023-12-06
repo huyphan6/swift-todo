0237e3337ba6a5b97eba1f6e3105b9c634d209cb
Huy Phan <phan.q.huy21@gmail.com>
2023-12-05 21:30:05 -0500

    Profile Page + Log Out function

--- todo/Views/MainView.swift
+++ todo/Views/MainView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct MainView: View {
     @StateObject var mainViewModel = MainViewViewModel()
     
-    // if the user is not signed in, show the Login View, otherwise show the todo view
+    /// if the user is not signed in, show the Login View, otherwise show the todo view
     var body: some View {
         if mainViewModel.isSignedIn && !mainViewModel.currentUserId.isEmpty {
-            // show the home page
+            /// show the home page
             accountView
         } else {
             LoginView()
--- todo/Views/ProfileView.swift
+++ todo/Views/ProfileView.swift
@@ -13,10 +13,65 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = profileViewModel.user {
+                    /// abstracts the view into a component like in react with a user prop
+                    profile(user: user)
+                }
+                else {
+                    Text("Loading Profile...")
+                }
+
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            profileViewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // show avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // show user info: name, email, member since
+        VStack (alignment: .leading){
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text("\(user.firstName + " " + user.lastName)")
+            }
+            .padding(1)
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text("\(user.email)")
+            }
+            .padding(1)
+            
+            HStack {
+                Text("Joined: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding(1)
+        }
+        .padding()
+        
+        Spacer()
+        
+        // sign out button
+        TDLButton(title: "Log Out", color: .red, action: profileViewModel.logout)
+            .frame(width: 100, height: 50, alignment: .center)
+            .padding()
+        
+        Spacer()
     }
 }
 
